35e120550223e9c033a5b88ef2d80bd3c3e2a239
VIJ4YKUMAR <vijayja07@gmail.com>
2022-09-18 18:32:04 +0530

    added profile picture

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './About.css';
+import img from './images/me.jpeg';
 
 //class about extends React.Component<any, any>{
     //constructor(props: any){
@@ -20,7 +21,7 @@ import './About.css';
         <div>
             <h1 className="title">About</h1>
             <div id = "preload">
-            <img src="images/me.jpeg" alt="profile"></img>
+            <img src={img} alt="profile" width="300" height="300"></img>
             </div>
             <p className="content">Hi <span role="img" aria-label="hello">👋</span> I'm Vijay. I was born and raised in Chennai. I did my undergrad in CSE from College Of Engineering Guindy, Anna University. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.</p> 
             <p className="content-2">I like to travel. I believe travelling gives you a new perspective to everything. You get to know a lot about the different cultures, the cuisines, their history, the language and all the small unique things every place has to offer. And I like to play cricket on weekends <span role="img" aria-label="winking">😉</span>.</p>
